a07c6fd97148aa78a055bf21d732a3311b72254e
gavriel <gbatyrov10@gmail.com>
2024-01-24 16:15:23 -0500

    finnished

---
+++ Fullstack_academy/test_project/test/index.js
@@ -0,0 +1,8 @@
+const script = document.createElement("script");
+script.src = "https://unpkg.com/aos@next/dist/aos.js";
+document.head.appendChild(script);
+
+// Initialize AOS
+script.onload = () => {
+  AOS.init();
+};
